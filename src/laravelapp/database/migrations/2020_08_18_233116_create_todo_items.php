16c5d07f7f906b50d515fde1560531d964b4653b
坂上　正 <t_sakagami@miraino-katachih2.co.jp>
2020-08-19 08:49:32 +0900

    マイグレーション作成、接続先DB変更。

---
+++ src/laravelapp/database/migrations/2020_08_18_233116_create_todo_items.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTodoItems extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todo_items', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8mb4';
+            $table->collation = 'utf8mb4_unicode_ci';
+            $table->bigIncrements('id');
+            $table->date('expiration_date');
+            $table->string('todo_item', 50);
+            $table->tinyInteger('is_completed')->default(0);
+            $table->tinyInteger('is_deleted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todo_items');
+    }
+}
